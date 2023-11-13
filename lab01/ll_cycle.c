326cf007b9accf20d8e084aacab67fe4c168507a
vboxuser <vboxuser@Machine1.myguest.virtualbox.org>
2023-11-12 20:44:01 -0500

    lab01 finished

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,19 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+    if (hare == NULL)
+    	return 0;
+    while(hare != NULL)
+    {
+    	hare = hare->next;
+	if (hare == NULL)
+		return 0;
+	hare = hare->next;
+	tortoise = tortoise->next;
+    	if (tortoise == hare)
+            return 1;
+    }
     return 0;
 }
