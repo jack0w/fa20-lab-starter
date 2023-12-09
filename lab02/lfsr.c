70e2e0557ad0155546b4666157975b9e9e294f2a
vboxuser <vboxuser@Machine1.myguest.virtualbox.org>
2023-12-09 17:23:29 -0500

    Lab02 complete

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,7 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t bit = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1;
+    *reg = (*reg >> 1) | (bit << 15);
 }
 
--- lab02/vector.c
+++ lab02/vector.c
@@ -19,6 +19,7 @@ static void allocation_failed() {
 }
 
 /* Bad example of how to create a new vector */
+// REASON: v is created in stack and it will be dropped after return. retval will point to undefined thing after return.
 vector_t *bad_vector_new() {
     /* Create the vector and a pointer to it */
     vector_t *retval, v;
@@ -36,6 +37,7 @@ vector_t *bad_vector_new() {
 }
 
 /* Another suboptimal way of creating a vector */
+// REASON: return a struct obj will make a copy of the obj. It's not efficient.
 vector_t also_bad_vector_new() {
     /* Create the vector */
     vector_t v;
@@ -58,27 +60,27 @@ vector_t *vector_new() {
     vector_t *retval;
 
     /* First, we need to allocate memory on the heap for the struct */
-    retval = /* YOUR CODE HERE */
+    retval = malloc(sizeof(vector_t));
 
     /* Check our return value to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
+    if (retval == NULL) {
         allocation_failed();
     }
 
     /* Now we need to initialize our data.
        Since retval->data should be able to dynamically grow,
        what do you need to do? */
-    retval->size = /* YOUR CODE HERE */;
-    retval->data = /* YOUR CODE HERE */;
+    retval->size = 1;
+    retval->data = malloc(sizeof(int));
 
     /* Check the data attribute of our vector to make sure we got memory */
-    if (/* YOUR CODE HERE */) {
-        free(retval);				//Why is this line necessary?
+    if (retval->data == NULL) {
+        free(retval);				//Why is this line necessary? A:Otherwise, memeroy of retval is not freed and the program exits.
         allocation_failed();
     }
 
     /* Complete the initialization by setting the single component to zero */
-    /* YOUR CODE HERE */ = 0;
+    retval->data[0] = 0;
 
     /* and return... */
     return retval;
@@ -96,8 +98,8 @@ int vector_get(vector_t *v, size_t loc) {
     /* If the requested location is higher than we have allocated, return 0.
      * Otherwise, return what is in the passed location.
      */
-    if (loc < /* YOUR CODE HERE */) {
-        return /* YOUR CODE HERE */;
+    if (loc < v->size) {
+        return v->data[loc];
     } else {
         return 0;
     }
@@ -106,7 +108,8 @@ int vector_get(vector_t *v, size_t loc) {
 /* Free up the memory allocated for the passed vector.
    Remember, you need to free up ALL the memory that was allocated. */
 void vector_delete(vector_t *v) {
-    /* YOUR SOLUTION HERE */
+    free(v->data);
+    free(v);
 }
 
 /* Set a value in the vector. If the extra memory allocation fails, call
@@ -117,4 +120,29 @@ void vector_set(vector_t *v, size_t loc, int value) {
      */
 
     /* YOUR SOLUTION HERE */
+
+    if(v == NULL) {
+        fprintf(stderr, "vector_get: passed a NULL vector.\n");
+        abort();
+    }
+
+    if (loc < v->size)
+    {
+    	v->data[loc] = value;
+    }
+    else
+    {
+    	int *dataCopy = calloc(loc + 1, sizeof(int));
+	if (dataCopy == NULL) {
+            allocation_failed();
+        }
+	for (int i = 0; i < v->size; i++)
+	    dataCopy[i] = v->data[i];
+
+	dataCopy[loc] = value;
+	free(v->data);
+	v->data = dataCopy;
+	v->size = loc + 1;
+    }
+
 }
